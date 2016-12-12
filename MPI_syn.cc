15e0719f5b347fcd578221329f7167d63668fa3d
zlsh80826 <zlsh80826@gmail.com>
2016-12-12 16:58:27 +0800

    runable version

--- MPI_syn.cc
+++ MPI_syn.cc
@@ -6,9 +6,12 @@ uint32_t vertex_num, edge_num;
 uint32_t user_threads;
 uint32_t source;
 int32_t Rank, nprocs;
+int32_t* parents;
 
 char* input_file;
 char* output_file;
+
+std::ofstream outfs;
 
 class Edge {
 public:
@@ -28,14 +31,32 @@ public:
     Vertex() : distance_to_source(std::numeric_limits<uint32_t>::max()) {}
 };
 
+void FindPath(uint32_t root) {
+    std::vector<uint32_t> reverse_vec;
+    uint32_t current = root;
+    reverse_vec.emplace_back(current);
+    while (current != source) {
+        current = parents[current];
+        reverse_vec.emplace_back(current);
+    }
+
+    std::reverse(reverse_vec.begin(), reverse_vec.end());
+
+    for (size_t i = 0; i < reverse_vec.size() - 1; ++i) {
+        outfs << reverse_vec[i] + 1 << " ";
+    }
+    outfs << reverse_vec[reverse_vec.size() - 1] + 1 << std::endl;
+}
+
 int main(int argc, char** argv) {
-    auto TstartTimer = std::chrono::high_resolution_clock::now();
+
+    // auto TstartTimer = std::chrono::high_resolution_clock::now();
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
     user_threads = std::stoi(argv[1]);
-    source = std::stoi(argv[4]);
+    source = std::stoi(argv[4]) - 1;
     input_file = argv[2];
     output_file = argv[3];
 
@@ -46,9 +67,9 @@ int main(int argc, char** argv) {
     int32_t* destinations;
     int32_t* weights;
 
-    auto TendTimer = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> Tdiff = TendTimer - TstartTimer;
-    std::cout << "Construct Graph Took " << Tdiff.count() << " Second" << std::endl;
+    // auto TendTimer = std::chrono::high_resolution_clock::now();
+    // std::chrono::duration<double> Tdiff = TendTimer - TstartTimer;
+    // std::cout << "Construct Graph Took " << Tdiff.count() << " Second" << std::endl;
 
     if (Rank == 0) {
         auto IstartTimer = std::chrono::high_resolution_clock::now();
@@ -89,7 +110,7 @@ int main(int argc, char** argv) {
         }
         auto IendTimer = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> Idiff = IendTimer - IstartTimer;
-        std::cout << "Construct Graph Took " << Idiff.count() << " Second" << std::endl;
+        std::cout << "Reading Input " << Idiff.count() << " Second" << std::endl;
 
         auto startTimer = std::chrono::high_resolution_clock::now();
         MPI_Dist_graph_create(MPI_COMM_WORLD, vertex_num, sources, degrees, destinations, weights, MPI_INFO_NULL, false, &graph_communicator);
@@ -103,8 +124,7 @@ int main(int argc, char** argv) {
         weights = nullptr;
         MPI_Dist_graph_create(MPI_COMM_WORLD, 0, sources, degrees, destinations, weights, MPI_INFO_NULL, false, &graph_communicator);
     }
-    // MPI_Barrier(graph_communicator);
-    int32_t old_rank = Rank;
+
     int32_t status;
     MPI_Topo_test(graph_communicator, &status);
     // std::cout << status << " " << MPI_DIST_GRAPH << std::endl;
@@ -112,7 +132,55 @@ int main(int argc, char** argv) {
     // std::cout << old_rank << " " << Rank << std::endl;
     int32_t id, od, we;
     MPI_Dist_graph_neighbors_count(graph_communicator, &id, &od, &we);
-    //printf("Rank: %d has %d indegree %d outdegree %d weights\n", Rank + 1, id, od, we);
+    // printf("Rank: %d has %d indegree %d outdegree %d weights\n", Rank + 1, id, od, we);
+
+    int32_t* clear_source = new int32_t[id];
+    int32_t* clear_source_weight = new int32_t[id];
+    int32_t* clear_dest = new int32_t[od];
+    int32_t* clear_dest_weight = new int32_t[od];
+    int32_t* neighbors_dest_to_source = new int32_t[id];
+
+    MPI_Dist_graph_neighbors(graph_communicator, id, clear_source, clear_source_weight, od, clear_dest, clear_dest_weight);
+
+    bool global_done = false;
+    int32_t distance_to_source = std::numeric_limits<int32_t>::max();
+    int32_t parent = source;
+    if ( source == Rank ) {
+        distance_to_source = 0;
+    }
+
+    while ( not global_done ) {
+        MPI_Neighbor_allgather(&distance_to_source, 1, MPI_INT, neighbors_dest_to_source, 1, MPI_INT, graph_communicator);
+        bool local_done = true;
+        for ( ssize_t i = 0; i < id; ++i) {
+            if (neighbors_dest_to_source[i] == std::numeric_limits<int32_t>::max())
+                continue;
+            const int32_t you_weight = neighbors_dest_to_source[i] + clear_dest_weight[i];
+            if ( you_weight < distance_to_source) {
+                distance_to_source = you_weight;
+                parent = clear_dest[i];
+                local_done = false;
+            }
+        }
+        MPI_Allreduce(&local_done, &global_done, 1, MPI::BOOL, MPI_LAND, MPI_COMM_WORLD);
+    }
+
+    if (Rank == 0)
+        parents = new int32_t[vertex_num];
+
+    MPI_Gather(&parent, 1, MPI_INT, parents, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (Rank == 0) {
+        outfs.open(output_file);
+
+        for (uint32_t i = 0; i < vertex_num; ++i) {
+            if (i == source)
+                outfs << source + 1 << " " << source + 1 << std::endl;
+            else
+                FindPath(i);
+        }
+        outfs.close();
+    }
 
     MPI_Finalize();
 }
